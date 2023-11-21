ff1ba76ce37313c17282f1c7358ded509c596dfb
Jakub Adamski <jakub.adamski12@gmail.com>
2023-11-21 15:37:59 +0000

    change

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+
+
 async function WeatherApp(){
 const response = await fetch('https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={APIKeY}');
 var data = await response.json();
